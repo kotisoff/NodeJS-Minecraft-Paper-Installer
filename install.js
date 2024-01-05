808ed39030989c93abda285f48545cb95faffbd4
Котя <71443736+kotisoff@users.noreply.github.com>
2024-01-05 16:45:38 +0300

    Bump to 0.2.0.
    - Версии paper теперь автоматически загружаются с поста osipxd и хранятся в процессе.
    - Чуть подправил некоторые аспекты в выводе в консоль.

--- install.js
+++ install.js
@@ -1,93 +1,194 @@
-// ======= Update URL from here: https://gist.github.com/osipxd/6119732e30059241c2192c4a8d2218d9 ======= //
-const paper_versions_url = "https://gist.githubusercontent.com/osipxd/6119732e30059241c2192c4a8d2218d9/raw/90ba4055686f2d7ca2cdfa98f463bfe7bdf24885/paper-versions.json"
-// ===================================================================================================== //
+import fs from "node:fs";
+import https from "node:https";
+import readline from "node:readline";
 
-import fs from "node:fs"
-import https from "node:https"
-import readline from "node:readline"
+console.clear();
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const prompt = (str) => new Promise(resolve => { rl.question(str, resolve) });
+const prompt = (str) =>
+  new Promise((resolve) => {
+    rl.question(str, resolve);
+  });
 
-const download = async(dest,url) => {
-    await new Promise(resolve => {
-        const file = fs.createWriteStream(dest);
-        https.get(url, res => {
-            var stream = res.pipe(file);
-            stream.on("drain",()=>{
-                console.log("Downloaded: "+Math.floor(stream.bytesWritten/1024)+"KB...")
-                process.stdout.moveCursor(0,-1)
-            })
-            stream.on("finish", () => {
-                process.stdout.clearLine()
-                console.log("Total file size: "+Math.floor(stream.bytesWritten/1024)+"KB")
-                file.close(resolve);
-            });
+/** @returns { Promise<string> } */
+const get_paper_versions_url = async () =>
+  await new Promise(async (resolve) => {
+    const githubusercontent = "https://gist.githubusercontent.com";
+    const regex =
+      /\/[A-Za-z]+\/[A-Za-z0-9]+\/raw\/[A-Za-z0-9]+\/paper-versions\.json/i;
+
+    https.get(
+      "https://gist.github.com/osipxd/6119732e30059241c2192c4a8d2218d9",
+      (res) => {
+        const data = [];
+
+        res.on("data", (buf) => {
+          data.push(buf.toString());
         });
-    })
-}
 
-let color = {
-    byNum: (mess, fgNum, bgNum) => {
-        mess = mess || '';
-        fgNum = fgNum === undefined ? 31 : fgNum;
-        bgNum = bgNum === undefined ? 49 : bgNum;
-        return '\u001b[' + fgNum + 'm' + '\u001b[' + bgNum + 'm' + mess + '\u001b[39m\u001b[49m';
-    },
-    black: (mess, fgNum) => color.byNum(mess, 30, fgNum),
-    red: (mess, fgNum) => color.byNum(mess, 31, fgNum),
-    green: (mess, fgNum) => color.byNum(mess, 32, fgNum),
-    yellow: (mess, fgNum) => color.byNum(mess, 33, fgNum),
-    blue: (mess, fgNum) => color.byNum(mess, 34, fgNum),
-    magenta: (mess, fgNum) => color.byNum(mess, 35, fgNum),
-    cyan: (mess, fgNum) => color.byNum(mess, 36, fgNum),
-    white: (mess, fgNum) => color.byNum(mess, 37, fgNum)
+        res.on("error", (e) => {
+          console.log(color.red("Error:\n" + e));
+          console.log(color.red("Process exit."));
+          process.exit(-1);
+        });
+
+        res.on("close", () => {
+          const page = data.join("");
+          resolve(githubusercontent + regex.exec(page)[0]);
+        });
+      }
+    );
+  });
+
+/** @returns { Promise<{latest:string, versions:[string]} | undefined> } */
+const get_json = async (link = "") =>
+  await new Promise(async (resolve) => {
+    https.get(link, (res) => {
+      const data = [];
+
+      res.on("data", (buf) => {
+        data.push(buf.toString());
+      });
+
+      res.on("error", (e) => {
+        console.log(color.red("Error:\n" + e));
+        console.log(color.red("Process exit."));
+        process.exit(-1);
+      });
+
+      res.on("close", () => {
+        const page = data.join("");
+        try {
+          resolve(JSON.parse(page));
+        } catch (e) {
+          console.log(color.red("Page is not an JSON!"), e);
+          resolve();
+        }
+      });
+    });
+  });
+
+const download = async (dest, url) => {
+  await new Promise((resolve) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, (res) => {
+      let stream = res.pipe(file);
+      stream.on("drain", () => {
+        console.log(
+          "Downloaded: " + Math.floor(stream.bytesWritten / 1024) + "KB..."
+        );
+        process.stdout.moveCursor(0, -1);
+      });
+      stream.on("finish", () => {
+        process.stdout.clearLine();
+        console.log(
+          "Total file size: " + Math.floor(stream.bytesWritten / 1024) + "KB"
+        );
+        file.close(resolve);
+      });
+    });
+  });
 };
 
-if (!fs.existsSync('./paper-versions.json')) {
-    console.log(color.cyan("Downloading \"paper-versions.json\"..."))
-    await download("paper-versions.json",paper_versions_url)
-}
-const paper = await JSON.parse(fs.readFileSync("./paper-versions.json"))
+let color = {
+  byNum: (mess, fgNum, bgNum) => {
+    mess = mess || "";
+    fgNum = fgNum === undefined ? 31 : fgNum;
+    bgNum = bgNum === undefined ? 49 : bgNum;
+    return (
+      "\u001b[" +
+      fgNum +
+      "m" +
+      "\u001b[" +
+      bgNum +
+      "m" +
+      mess +
+      "\u001b[39m\u001b[49m"
+    );
+  },
+  black: (mess, fgNum) => color.byNum(mess, 30, fgNum),
+  red: (mess, fgNum) => color.byNum(mess, 31, fgNum),
+  green: (mess, fgNum) => color.byNum(mess, 32, fgNum),
+  yellow: (mess, fgNum) => color.byNum(mess, 33, fgNum),
+  blue: (mess, fgNum) => color.byNum(mess, 34, fgNum),
+  magenta: (mess, fgNum) => color.byNum(mess, 35, fgNum),
+  cyan: (mess, fgNum) => color.byNum(mess, 36, fgNum),
+  white: (mess, fgNum) => color.byNum(mess, 37, fgNum),
+};
 
-console.log(color.green('Paper installer for Termux | v0.1'));
-console.log(color.yellow('Latest paper for now:'), paper.latest);
-console.log(color.cyan('Leave prompt blank to show versions.'));
-console.log(color.cyan('Type "exit" or "e" to leave process'));
+const updateData = async () => {
+  process.stdout.write("Getting: url");
+  const url = await get_paper_versions_url();
+  process.stdout.moveCursor(-3);
+  process.stdout.write("json");
+  const paper = await get_json(url);
+  if (!paper) {
+    console.log(color.red("\nPaper list is unavalible.\nProcess exit."));
+    process.exit();
+  }
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  console.log(color.green("Getting: done.\n"));
+  return { url, paper };
+};
+
+let tmpdata = { url: "", paper: { latest: "", versions: [""] }, done: false };
+
+const text = [
+  color.green("Paper installer for Termux | v0.2"),
+  color.cyan('Type "exit" or "e" to leave process'),
+];
+text.forEach((i) => console.log(i));
+
 const checkPrompt = async () => {
-    let ver = await prompt("Enter version to install: ") ?? "";
-    if (ver == 'exit' || ver == 'e' || ver == null) return rl.close();
-    let text = [], check = false
-    for (let v in paper.versions) {
-        if (ver == v) check = true
-        text.push(v);
-    };
-    if (!check) {
-        text = text.join(', ');
-        console.log(color.green('Versions avalible: '), text);
-        return checkPrompt();
-    }
-    else {
-        console.log(color.magenta('Downloading Paper ' + ver + '...'));
-        try {
-            fs.mkdirSync('../mcserver');
-        } catch {
-            rl.close()
-            return console.log("Files already exist.")
-        }
-        await download('../mcserver/server.jar',paper.versions[ver])
-        fs.writeFileSync("../mcserver/start.sh", "java -jar server.jar\npause", e => { });
-        fs.writeFileSync("../mcserver/start.bat", "@echo off\njava -jar server.jar\npause", e => { });
-        console.log(color.green('Paper downloaded successfully!'));
-        rl.close()
-    }
-}
+  const { url, paper } = !tmpdata.done ? await updateData() : tmpdata;
+  tmpdata.url = url;
+  tmpdata.paper = paper;
+  tmpdata.done = true;
+  const versions = Object.keys(paper.versions);
+
+  console.log(color.yellow("Latest paper:"), paper.latest);
+  console.log(color.green("Versions avalible:"), versions.join(", "));
+
+  let ver = (await prompt("Enter version to install: ")) ?? "";
+
+  if (ver == "exit" || ver == "e" || ver == null) return rl.close();
+  if (ver == "latest") ver = paper.latest;
+  if (!versions.includes(ver)) {
+    console.clear();
+    text.forEach((i) => console.log(i));
+    console.log(color.cyan("Write down any version, please!\n"));
+    return checkPrompt();
+  }
+
+  console.log(color.magenta("Downloading Paper " + ver + "..."));
+
+  try {
+    fs.mkdirSync("../mcserver");
+  } catch {
+    rl.close();
+    return console.log("Files already exist.");
+  }
+  await download("../mcserver/server.jar", paper.versions[ver]);
+  fs.writeFileSync(
+    "../mcserver/start.sh",
+    "java -jar server.jar\npause",
+    (e) => {}
+  );
+  fs.writeFileSync(
+    "../mcserver/start.bat",
+    "@echo off\njava -jar server.jar\npause",
+    (e) => {}
+  );
+  console.log(color.green("Paper downloaded successfully!"));
+  rl.close();
+};
 checkPrompt();
 
-// Thanks
-//          @dustinpfister for ansi colors function
-//          @osipxd for "paper-versions.json" file
+// Thanks to:
+//   • @dustinpfister for ansi colors function
+//   • @osipxd for "paper-versions.json"
